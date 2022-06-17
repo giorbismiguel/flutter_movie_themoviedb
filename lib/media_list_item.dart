c07babb3b6a2a26e7968fd74a7c30eeaafa0f180
Giorbis Miguel <giorbismiguel@gmail.com>
2022-06-17 12:47:13 -0400

    Show Image and add a new Positioned

--- lib/media_list_item.dart
+++ lib/media_list_item.dart
@@ -11,13 +11,35 @@ class MediaListItem extends StatelessWidget {
     return new Card(
       child: new Column(
         children: <Widget>[
-          new FadeInImage.assetNetwork(
-            placeholder: "assets/placeholder.jpg",
-            image: media.getBackDorpUrl(),
-            fit: BoxFit.cover,
-            height: 200.0,
-            width: double.infinity,
-            fadeInDuration: new Duration(milliseconds: 40),
+          new Container(
+            child: new Stack(
+              children: <Widget>[
+                new FadeInImage.assetNetwork(
+                  placeholder: "assets/placeholder.jpg",
+                  image: media.getBackDorpUrl(),
+                  fit: BoxFit.cover,
+                  height: 200.0,
+                  width: double.infinity,
+                  fadeInDuration: new Duration(milliseconds: 40),
+                ),
+
+                new Positioned(
+                  left: 0.0,
+                  bottom: 0.0,
+                  right: 0.0,
+                  child: new Container(
+                    width: double.infinity,
+                    height: 40.0,
+                    decoration: new BoxDecoration(
+                      color: Colors.grey[900]!.withOpacity(0.5),
+                    ),
+                    constraints: new BoxConstraints.expand(
+                      height: 55.0,
+                    ),
+                  ),
+                )
+              ],
+            ),
           )
         ],
       ),
